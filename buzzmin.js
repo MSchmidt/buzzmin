4345e8c11c41dcca9a6c21a67c929d27cf595da2
Matthias Schmidt <mail@m-schmidt.eu>
2011-05-09 20:25:46 +0200

    Cleanup markup and get random 5

--- buzzmin.js
+++ buzzmin.js
@@ -7,41 +7,44 @@ if (window.location.hash) {
 }
 
 $(document).bind("mobileinit", function(){
-	$.ajax({
-		url: 'http://buzzwords.tladesignz.com/data.pl',
-		dataType: 'jsonp',
-		success: function(data) {
-			console.log('...');
-			
-			var items = [];
-			
-			$.each(data, function(key, val) {
-				items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
+  $.ajax({
+    url: 'http://buzzwords.tladesignz.com/data.pl',
+    dataType: 'jsonp',
+    success: function(data) {
+      console.log('...');
 
-				var detail_page = '\
-				<div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
-      		<div data-role="header">\
-      			<h1>' + key + '</h1>\
-      		</div>\
-      		<div data-role="content">\
-      			<div data-role="fieldcontain">\
-      			 	<fieldset data-role="controlgroup">';
-        			 	$.each(val, function(key2, val2){
+      var items = [];
+
+      $.each(data, function(key, val) {
+        items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
+
+        var detail_page = '\
+        <div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
+          <div data-role="header">\
+            <h1>' + key + '</h1>\
+          </div>\
+          <div data-role="content">\
+            <div data-role="fieldcontain">\
+               <fieldset data-role="controlgroup">';
+                 reduced_val = val.sort(function(){
+                   return Math.round(Math.random())-0.5;
+                 }).slice(0, 5);
+                 $.each(reduced_val, function(key2, val2){
                   detail_page += '\
                   <input type="checkbox" name="checkbox-' + key2 +'" id="checkbox-' + key2 + '" />\
-        					<label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
-        				});
-      			  detail_page += '</fieldset>\
-      			</div>\
-      		</div>\
-      		<div data-role="footer">\
-      			<h4>Page Footer</h4>\
-      		</div>\
-      	</div>';
-      	$(detail_page).appendTo('body');
-			});
-			$(items.join('')).appendTo('#mainlist');
-			$('#mainlist').listview('refresh');
-		}
-	});
+                  <label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
+                });
+              detail_page += '</fieldset>\
+            </div>\
+          </div>\
+          <div data-role="footer">\
+            <h4>&copy; A. Alfarè &amp; M. Schmidt</h4>\
+          </div>\
+        </div>';
+        $(detail_page).appendTo('body');
+      });
+      $(items.join('')).appendTo('#mainlist');
+      $('#mainlist').listview('refresh');
+    }
+  });
 });
