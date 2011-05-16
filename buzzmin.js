91ac47ceb4fbdb2d6e7753ffa383e8aabdb33451
Matthias Schmidt <mail@m-schmidt.eu>
2011-05-16 17:40:03 +0200

    Amplify storage

--- buzzmin.js
+++ buzzmin.js
@@ -6,43 +6,66 @@ if (window.location.hash) {
   window.location.pathname;
 }
 
-$(document).bind("mobileinit", function(){
+$(document).bind('ready', function(){
+  if (!amplify.store('buzzwords')) {
+    $.ajax({
+      url: 'http://buzzwords.tladesignz.com/data.pl',
+      dataType: 'jsonp',
+      success: function(data) {
+        amplify.store('buzzwords', data);
+        buzzwords = data;
+        console.log('load from net');
+        render_layout(buzzwords);
+      }
+    });
+  } else {
+    console.log('load from storage');
+    render_layout(amplify.store('buzzwords'));
+  }
+
+  function render_layout(buzzwords) {
+    var items = [];
+
+    $.each(buzzwords, function(key, val) {
+      items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
+
+      var detail_page = '\
+      <div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
+        <div data-role="header">\
+          <h1>' + key + '</h1>\
+        </div>\
+        <div data-role="content">\
+          <div data-role="fieldcontain">\
+             <fieldset data-role="controlgroup">';
+               reduced_val = val.sort(function(){
+                 return Math.round(Math.random())-0.5;
+               }).slice(0, 5);
+               $.each(reduced_val, function(key2, val2){
+                detail_page += '\
+                <input type="checkbox" name="checkbox-' + key2 +'" id="checkbox-' + key2 + '" data-group="' + key + '" />\
+                <label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
+              });
+            detail_page += '</fieldset>\
+          </div>\
+        </div>\
+        <div data-role="footer">\
+          <h4>&copy; A. Alfarè &amp; M. Schmidt</h4>\
+        </div>\
+      </div>';
+      $(detail_page).appendTo('body');
+    });
+    $(items.join('')).appendTo('#mainlist');
+    $('#mainlist').listview('refresh');
+  }
+  
+  
+  /*
+  
   $.ajax({
     url: 'http://buzzwords.tladesignz.com/data.pl',
     dataType: 'jsonp',
     success: function(data) {
-      var items = [];
-
-      $.each(data, function(key, val) {
-        items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
-
-        var detail_page = '\
-        <div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
-          <div data-role="header">\
-            <h1>' + key + '</h1>\
-          </div>\
-          <div data-role="content">\
-            <div data-role="fieldcontain">\
-               <fieldset data-role="controlgroup">';
-                 reduced_val = val.sort(function(){
-                   return Math.round(Math.random())-0.5;
-                 }).slice(0, 5);
-                 $.each(reduced_val, function(key2, val2){
-                  detail_page += '\
-                  <input type="checkbox" name="checkbox-' + key2 +'" id="checkbox-' + key2 + '" data-group="' + key + '" />\
-                  <label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
-                });
-              detail_page += '</fieldset>\
-            </div>\
-          </div>\
-          <div data-role="footer">\
-            <h4>&copy; A. Alfarè &amp; M. Schmidt</h4>\
-          </div>\
-        </div>';
-        $(detail_page).appendTo('body');
-      });
-      $(items.join('')).appendTo('#mainlist');
-      $('#mainlist').listview('refresh');
+      
     }
   });
 
@@ -53,4 +76,5 @@ $(document).bind("mobileinit", function(){
       //document.getElementById('soundContainer').play();
     }
   });
+  */
 });
