83aa9266356687c0da4b09dc78defb4a99395d95
Andreas Alfarè <andreas.alfare@gmail.com>
2011-05-16 17:31:04 +0200

    local storage

--- buzzmin.js
+++ buzzmin.js
@@ -7,44 +7,60 @@ if (window.location.hash) {
 }
 
 $(document).bind("mobileinit", function(){
-  $.ajax({
-    url: 'http://buzzwords.tladesignz.com/data.pl',
-    dataType: 'jsonp',
-    success: function(data) {
-      var items = [];
+	var buzzwords = window.localStorage.getItem('buzzwords');
+	
+	if(buzzwords) {
+		console.log('from storage');
+		render(JSON.parse(buzzwords));
+	}
+	else {
+		$.ajax({
+	    url: 'http://buzzwords.tladesignz.com/data.pl',
+	    dataType: 'jsonp',
+	    success: function(data) {
+		    console.log('from net');
+				window.localStorage.setItem('buzzwords', JSON.stringify(data));
+				render(data);
+			}
+	  });
+	}
 
-      $.each(data, function(key, val) {
-        items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
+	function render(data) {
+		console.log(data);
+		
+		var items = [];
 
-        var detail_page = '\
-        <div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
-          <div data-role="header">\
-            <h1>' + key + '</h1>\
+    $.each(data, function(key, val) {
+      items.push('<li id="' + key + '"><a href="#detail-' + key + '">' + key + '</a></li>');
+
+      var detail_page = '\
+      <div data-role="page" data-theme="b" id="detail-' + key + '" data-url="detail-' + key + '">\
+        <div data-role="header">\
+          <h1>' + key + '</h1>\
+        </div>\
+        <div data-role="content">\
+          <div data-role="fieldcontain">\
+             <fieldset data-role="controlgroup">';
+               reduced_val = val.sort(function(){
+                 return Math.round(Math.random())-0.5;
+               }).slice(0, 5);
+               $.each(reduced_val, function(key2, val2){
+                detail_page += '\
+                <input type="checkbox" name="checkbox-' + key2 +'" id="checkbox-' + key2 + '" data-group="' + key + '" />\
+                <label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
+              });
+            detail_page += '</fieldset>\
           </div>\
-          <div data-role="content">\
-            <div data-role="fieldcontain">\
-               <fieldset data-role="controlgroup">';
-                 reduced_val = val.sort(function(){
-                   return Math.round(Math.random())-0.5;
-                 }).slice(0, 5);
-                 $.each(reduced_val, function(key2, val2){
-                  detail_page += '\
-                  <input type="checkbox" name="checkbox-' + key2 +'" id="checkbox-' + key2 + '" data-group="' + key + '" />\
-                  <label for="checkbox-' + key2 + '" data-theme="c">' + val2 + '</label>';
-                });
-              detail_page += '</fieldset>\
-            </div>\
-          </div>\
-          <div data-role="footer">\
-            <h4>&copy; A. Alfarè &amp; M. Schmidt</h4>\
-          </div>\
-        </div>';
-        $(detail_page).appendTo('body');
-      });
-      $(items.join('')).appendTo('#mainlist');
-      $('#mainlist').listview('refresh');
-    }
-  });
+        </div>\
+        <div data-role="footer">\
+          <h4>&copy; A. Alfarè &amp; M. Schmidt</h4>\
+        </div>\
+      </div>';
+      $(detail_page).appendTo('body');
+    });
+    $(items.join('')).appendTo('#mainlist');
+    $('#mainlist').listview('refresh');
+	}
 
   $('input[type="checkbox"]').live('change', function(e){
     if ($('input[data-group=' + $(this).attr('data-group') + ']:checked').length == 5){
